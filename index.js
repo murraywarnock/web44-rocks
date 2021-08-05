4fb55b49bd8e6d69e5917e5ab97a63cc9b128917
MurrayWarnock <murray.warnock@gmail.com>
2021-08-05 11:32:33 -0600

    new push to check port

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('web 44 rocks');
+require('dotenv').config(); //allows to inject fake env vars
+const PORT = process.env.PORT || 5000;
+// const { PORT } = require('./comfig');
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Web 44 Is Awesome!</h1>`)
+})
+
+server.use('*', (req, res) => {
+    res.json({
+        message: 'web 44 is awsome indeed'
+    })
+})
+
+server.listen(PORT, () => { //heroku machine uses a different port
+    console.log(`listening on ${PORT}`)
+});
+
